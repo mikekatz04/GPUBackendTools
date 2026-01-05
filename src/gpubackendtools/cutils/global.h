b0a60b4a56c64057cee031fa77d984fc7bad54cb
mikekatz04 <mikekatz04@gmail.com>
2026-01-05 13:43:53 -0800

    continued updates

--- src/gpubackendtools/cutils/global.h
+++ src/gpubackendtools/cutils/global.h
@@ -1,26 +1,4 @@
-/*  This code was edited by Michael Katz. The information in this file is
- originally from the LAL library.
- *  The original copyright and license is shown below. Michael Katz has edited
- and added to
- *  the code for his purposes and removed dependencies on the LAL libraries.
- *  This code is distrbuted under the same GNU license it originally came with.
-
- *  Copyright (C) 2017 Sebastian Khan, Francesco Pannarale, Lionel London
- *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with with program; see the file COPYING. If not, write to the
- *  Free Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *  MA  02111-1307  USA
+/*  
  */
 
 #ifndef _GLOBAL_HEADER_
@@ -41,6 +19,7 @@
 #define CUDA_KERNEL __global__
 #define CUDA_SHARED __shared__
 #define CUDA_SYNCTHREADS __syncthreads();
+#define CUDA_THREAD_ZERO (threadIdx.x == 0)
 
 /*
 Function for gpu Error checking.
@@ -64,6 +43,7 @@ inline void gpuAssert(cudaError_t code, const char* file, int line,
 #define CUDA_KERNEL
 #define CUDA_SHARED
 #define CUDA_SYNCTHREADS
+#define CUDA_THREAD_ZERO (true)
 #endif
 
 typedef gcmplx::complex<double> cmplx;
